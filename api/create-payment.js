ac421c5442b6a67b7a9ab5a911461280e14ddaf9
bizmedia1 <victorisrael539@gmail.com>
2026-03-07 19:15:49 +0100

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -2,6 +2,9 @@ export default async function handler(req, res) {
 
 const secret = process.env.KORAPAY_SECRET;
 
+/* generate unique reference */
+const reference = "ref_" + Date.now();
+
 const response = await fetch("https://api.korapay.com/merchant/api/v1/charges/initialize", {
 method: "POST",
 headers: {
@@ -9,12 +12,13 @@ headers: {
 Authorization: `Bearer ${secret}`
 },
 body: JSON.stringify({
+reference: reference,
 amount: 14500,
 currency: "NGN",
-redirect_url: "https://celestesuccesspage.uwu.ai",
+redirect_url: "https://yoursite.com/success",
 customer: {
 name: "Customer",
-email: "kbigbizz@email.com"
+email: "customer@email.com"
 }
 })
 });
@@ -23,4 +27,4 @@ const data = await response.json();
 
 res.status(200).json(data);
 
-} 
+}
