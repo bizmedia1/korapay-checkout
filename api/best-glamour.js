005ef5e957601fc6b4b1479b4e8fa992080d32f7
bizmedia1 <victorisrael539@gmail.com>
2026-04-09 16:22:24 +0100

    Update best-glamour.js

--- api/best-glamour.js
+++ api/best-glamour.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.korapay.com/merchant/api/v1/charges/initialize", {
       method: "POST",
       headers: {
-        Authorization: "Bearer " + process.env.KORA_SECRET_KEY",
+        Authorization: "Bearer " + process.env.KORA_SECRET_KEY,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
